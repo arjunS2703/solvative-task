23335c0ff2e381789add8d6746aa6754139d4d48
arjunS2703 <95018011+arjunS2703@users.noreply.github.com>
2023-06-27 21:43:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Search from './components/SearchElement/Search';
+import Table from './components/Table/Table';
+function App() {
+  return (
+    <div>
+      <Search />
+      <Table />
+    </div>
+  );
+}
+
+export default App;
